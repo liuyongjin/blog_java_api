ec6c55154e30edd6364efed0494babf1ba6f86a6
liuyongjin <YongJin_Liu@jabil.com>
2022-08-09 22:33:05 +0800

    新增tag接口

---
+++ src/main/java/com/example/demo/controller/TagController.java
@@ -0,0 +1,55 @@
+package com.example.demo.controller;
+
+import com.example.demo.common.ApiResponse;
+import com.example.demo.common.Constant.CommonEnum;
+import com.example.demo.model.dto.ArticleDTO;
+import com.example.demo.model.dto.GetArticleDTO;
+import com.example.demo.service.ArticleService;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+
+@RestController
+@RequestMapping("/article")
+@Validated
+public class ArticleController {
+
+    @Autowired
+    ArticleService articleService;
+
+    /**
+     * 查询文章列表
+     */
+    @RequestMapping("/list")
+    public ApiResponse getArticleList(@Valid GetArticleDTO getArticleDTO) {
+        PageInfo articleList = articleService.getArticleList(getArticleDTO.getPageIndex(), getArticleDTO.getPageSize());
+        return ApiResponse.success(articleList);
+    }
+
+    @PostMapping("/add")
+    public ApiResponse addArticle(@Validated(ArticleDTO.Insert.class) ArticleDTO articleDTO) {
+        Integer response = articleService.addArticle(articleDTO);
+        return ApiResponse.success(response);
+    }
+
+    @PostMapping("/del")
+    public ApiResponse delArticle(@NotNull(message = "id不能为空") Integer id) {
+        Integer response = articleService.delArticle(id);
+        return ApiResponse.success(CommonEnum.CREATE_SUCCESS.getStatus(), CommonEnum.CREATE_SUCCESS.getMsg(), response);
+    }
+
+
+    @PostMapping("/bdel")
+    public ApiResponse batchDelArticle(@NotNull(message = "ids不能为空") Integer[] ids) {
+        Integer response = articleService.batchDelArticle(ids);
+        return ApiResponse.success(CommonEnum.CREATE_SUCCESS.getStatus(), CommonEnum.CREATE_SUCCESS.getMsg(), response);
+    }
+}
